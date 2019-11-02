41ba569b6bb972facd8259bfe8c853e2643f68ec
GedorN <gedor.silvaneto@gmail.com>
2019-11-01 23:36:42 -0300

    Adição de sidedrawer

--- App.js
+++ App.js
@@ -9,6 +9,7 @@ import {
 } from 'react-native';
 
 import Home from './android/app/src/components/Home';
+import MenuDrawer from "react-native-side-drawer";
 const width = Dimensions.get('screen').width;
 
 export default class App extends React.Component {
@@ -17,21 +18,57 @@ export default class App extends React.Component {
     this.state = {
       postText: '',
       postImages: [],
+      open: false,
     };
   }
 
+  toggleOpen = () => {
+    this.setState({ open: !this.state.open });
+  };
+
+  openModal = () => {
+    this.setState({open: true});
+  }
+
+  closeModal = () => {
+    this.setState({open: false});
+  }
+
+  drawerContent = () => {
+    return (
+      <TouchableOpacity onPress={this.closeModal.bind(this)} style={styles.animatedBox}>
+        <Text>Close</Text>
+      </TouchableOpacity>
+    );
+          //TODO tirar margem do topo
+  };
+
+
   render() {
     return (
-      <View>
-        <View style={styles.header}>
-          <TouchableOpacity>
-              <Image source={require('./assets/images/bars-solid.png')} style={{width: 30, height: 30, tintColor: 'white'}}/>
-          </TouchableOpacity>
-          <Text style={{color: 'white', fontSize: 24, marginLeft: 100}}>
-            Spotted
-          </Text>
-        </View>
-        <Home/>
+        <View style={styles.container}>
+          <MenuDrawer
+            open={this.state.open}
+            drawerContent={this.drawerContent()}
+            drawerPercentage={60}
+            animationTime={250}
+            overlay={true}
+            opacity={0.4}
+            style={{margin: 0, padding: 0, width: 0, height: 0, display: 'none'}}
+          >
+            <View style={styles.header}>
+              <TouchableOpacity onPress={this.openModal.bind(this)}>
+                  <Image source={require('./assets/images/bars-solid.png')} style={{width: 30, height: 30, tintColor: 'white'}}/>
+              </TouchableOpacity>
+              <Text style={{color: 'white', fontSize: 24, marginLeft: 100}}>
+                Spotted
+              </Text>
+            </View>
+            <View>
+              <Home/>
+            </View>
+          </MenuDrawer>
+
       </View>
     );
   }
@@ -39,7 +76,12 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: 20,
+    marginTop: 0,
+    padding: 0,
+    margin: 0,
+    flex: 1,
+    zIndex: 0,
+    // backgroundColor:'blue'
   },
   header: {
     width: width,
@@ -50,6 +92,18 @@ const styles = StyleSheet.create({
     // alignItems: 'center',
     flexDirection: 'row',
     padding: 5,
-    paddingHorizontal: 10
+    margin: 0,
+    paddingHorizontal: 10,
+  },
+  animatedBox: {
+    flex: 1,
+    backgroundColor: "#38C8EC",
+  },
+  body: {
+    flex: 1,
+    margin: 0,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#F04812'
   }
 });
