aeebd0b89939fad06311a0da81e33e28e9510b6f
GedorN <gedor.silvaneto@gmail.com>
2020-05-13 17:27:37 -0300

    Correções pontuais de reatividade

--- android/app/src/components/CustomizationOptions.js
+++ android/app/src/components/CustomizationOptions.js
@@ -30,12 +30,12 @@ export default class CustomizationOptions extends React.Component {
     }
 
     componentDidMount(): void {
-/* 
+/*
         console.warn("PRODUCT CUSTOMIZATION", this.props.productCustomization);
         console.warn("CUSTOM",this.props.custom.label);
         console.warn("colors RECEIVED",this.props.colors); */
         this.props.productCustomization.forEach(item => {
-            
+
             if(item === this.props.custom.label){
                 this.state.i = this.props.productCustomization.indexOf(item);
                 /* console.warn("state i"  ,this.state.i); */
@@ -47,53 +47,58 @@ export default class CustomizationOptions extends React.Component {
     getOption = (item) => {
        /*  console.warn("item",item); */
         this.state.color = item;
+        this.setState({ color: item });
        /*  console.warn("STATE COLOR",this.state.color); */
        this.props.customizationCallback(this.state.i,this.i);
-      
+
     }
 
-   
+
 
 
     render = () => {
         return (
             <View style={{alignSelf:'center'}}>
-                {this.props.custom.field === 'select'?(
+                {
+                	this.props.custom.field === 'select' &&
                     <View style = {{width:theme.width * 0.50}}>
                         <Text style = {{ fontWeight:'bold' ,fontSize:20,alignSelf:'center',marginTop:25,marginBottom:5}}>{this.props.custom.label}</Text>
-                        {this.props.custom.options.map(i =>
-                        <TouchableOpacity style = {{marginTop:25,justifyContent:'center'}} onPress = {this.getOption.bind(this,i)}>
-                            <View  key={i} >
-                                <Text style = {{fontSize:17,marginBottom:22,alignSelf:'center',fontWeight:'bold',color:(this.state.color === i? this.state.color1 : this.state.color2)}}>{i}</Text>
-                            </View>
-                        </TouchableOpacity>
+                        {
+                        	this.props.custom.options.map(i =>
+		                        <TouchableOpacity style = {{marginTop:25,justifyContent:'center'}} onPress = {this.getOption.bind(this,i)}>
+		                            <View  key={i} >
+		                                <Text style = {{fontSize:17,marginBottom:22,alignSelf:'center',fontWeight:'bold',color:(this.state.color === i? this.state.color1 : this.state.color2)}}>{i}</Text>
+		                            </View>
+		                        </TouchableOpacity>
                             )}
-                        
-                    </View>
-                ): null }
 
-                {this.props.custom.field === 'radio'? (
+                    </View>
+                }
+
+                {
+                	this.props.custom.field === 'radio' &&
                     <View style = {{width:theme.width * 0.50}}>
                         <Text style = {{ fontWeight:'bold' ,fontSize:20,alignSelf:'center',marginTop:15,marginBottom:this.props.custom.options.length > 2 ? 10 : 50}}>{this.props.custom.label}</Text>
-                        {this.props.custom.options.map(i => 
-                            <View key={i}>
-                                <View style = {{marginBottom:25,justifyContent:'center'}}>
-                                    <Text style = {{fontSize:17,marginBottom:11,marginTop:11,alignSelf:'center',color:'#8f8f8f',fontWeight:'bold'}}>{i}</Text>
-                                   <View style = {{ width:theme.width * 0.1,alignSelf:'center'}}>
-                                        <RadioButton
-                                            color  = {this.props.colors[0]}
-                                            value = {i}
-                                            status={this.state.checked ===  i ? 'checked' : 'unchecked'}
-                                            onPress={() => { this.setState({ checked: i }); }}
-                                            />
-                                    </View>
-                                </View>
-                            </View>
+                        {
+                        	this.props.custom.options.map(i =>
+	                            <View key={i}>
+	                                <View style = {{marginBottom:25,justifyContent:'center'}}>
+	                                    <Text style = {{fontSize:17,marginBottom:11,marginTop:11,alignSelf:'center',color:'#8f8f8f',fontWeight:'bold'}}>{i}</Text>
+	                                   <View style = {{ width:theme.width * 0.1,alignSelf:'center'}}>
+	                                        <RadioButton
+	                                            color  = {this.props.colors[0]}
+	                                            value = {i}
+	                                            status={this.state.checked ===  i ? 'checked' : 'unchecked'}
+	                                            onPress={() => { this.setState({ checked: i }); }}
+	                                            />
+	                                    </View>
+	                                </View>
+	                            </View>
                             )}
                     </View>
-                ): null}
+                }
 
-             
+
             </View>
         );
     }
--- android/app/src/components/ProductScreen.js
+++ android/app/src/components/ProductScreen.js
@@ -43,23 +43,36 @@ export default class ProductScreen extends React.Component {
 			this.setState({product: resolve});
 			this.setState({productImages: resolve.images});
 
-			resolve.customization.forEach(element => {
-				if(element.field === 'select'){
-					this.state.selectOptions.push(element);
+			for(let i = 0; i < resolve.customization.length; i++) {
+				this.state.productCustomization.push(resolve.customization[i].label);
+				if(resolve.customization[i].field === 'select'){
+					this.state.selectOptions.push(resolve.customization[i]);
 				}
-				else if(element.field == 'radio'){
-					this.state.radioOptions.push(element);
+				else if(resolve.customization[i].field == 'radio'){
+					this.state.radioOptions.push(resolve.customization[i]);
 				}
-				else if(element.field == 'textArea'){
-					this.state.textAreaOptions.push(element);
+				else if(resolve.customization[i].field == 'textArea'){
+					this.state.textAreaOptions.push(resolve.customization[i]);
 				}
-			});
- 
-		 	this.state.product.customization.forEach(item => {
+			}
 
-				this.state.productCustomization.push(item.label);
-				
-			})
+			// resolve.customization.forEach(element => {
+			// 	if(element.field === 'select'){
+			// 		this.state.selectOptions.push(element);
+			// 	}
+			// 	else if(element.field == 'radio'){
+			// 		this.state.radioOptions.push(element);
+			// 	}
+			// 	else if(element.field == 'textArea'){
+			// 		this.state.textAreaOptions.push(element);
+			// 	}
+			// });
+
+		 	// this.state.product.customization.forEach(item => {
+			//
+			// 	this.state.productCustomization.push(item.label);
+			//
+			// })
 			/* console.warn("PRODUCT CUSTOMIZATION", this.state.productCustomization);  */
 		})
 
@@ -72,7 +85,7 @@ export default class ProductScreen extends React.Component {
             </View>
         );
 	}
-	
+
 	renderFooter = () => {
 		if (this.state.comments && this.state.comments.length > 0 && !this.state.endPulling) {
 			return (
@@ -88,118 +101,112 @@ export default class ProductScreen extends React.Component {
 		this.state.productCustomization[i] = option;
 	}
 
-	render() {																																																			
+	render() {
 		return (
 			<View style={{flex: 1}}>
 				<View >
                 <FlatList
-				ListHeaderComponent = {() =>
-				<View>
-					<View
-						style = {{width:170,height:100,alignSelf:'center',justifyContent:'center'}}>
-						<Image
-							style = {{width:90,height:70,alignSelf:'center'}}
-							source={{ uri:this.state.product? this.state.product.logo : null}}>
-						</Image>
-					</View>
-					<View style = {{marginTop:20,marginBottom:15}}>
-						<Text 
-						style = {{fontWeight:'bold',fontSize:25, alignSelf:'center'}}
-						> {this.state.product? this.state.product.name : null}</Text>
-					</View>
+					ListHeaderComponent = {() =>
+						<View>
+							<View
+								style = {{width:170,height:100,alignSelf:'center',justifyContent:'center'}}>
+								<Image
+									style = {{width:90,height:70,alignSelf:'center'}}
+									source={{ uri:this.state.product? this.state.product.logo : null}}>
+								</Image>
+							</View>
+							<View style = {{marginTop:20,marginBottom:15}}>
+								<Text
+								style = {{fontWeight:'bold',fontSize:25, alignSelf:'center'}}
+								> {this.state.product? this.state.product.name : null}</Text>
+							</View>
 
-					<Carousel
-						activePageIndicatorStyle = {{backgroundColor:this.state.product ? this.state.product.colors[0] : 'black'}}
-						autoplay
-						autoplayTimeout={5000}
-						loop
-						index={0}
-						pageSize={theme.width}
-					>
-						{this.state.productImages? this.state.productImages.map((image, index) => this.renderPage(image, index)) :null}
-					</Carousel> 
+							<Carousel
+								activePageIndicatorStyle = {{backgroundColor:this.state.product ? this.state.product.colors[0] : 'black'}}
+								autoplay
+								autoplayTimeout={5000}
+								loop
+								index={0}
+								pageSize={theme.width}
+							>
+								{this.state.productImages? this.state.productImages.map((image, index) => this.renderPage(image, index)) :null}
+							</Carousel>
 
-					<View style = {{width:theme.width * 0.9, /* borderColor: 'black',borderWidth:1 */alignSelf:'center'}}>
-						<Text 
-						style = {{color:'#8f8f8f', fontWeight:'bold',fontSize:19,marginBottom:4}} 
-						>{'Valor:'}
-						</Text>
-						<View style = {{flexDirection:'row'}}>
-							<Text
-							style = {{fontWeight:'bold',fontSize:17}}
-							>{'R$ ' + (this.state.product? this.state.product.price:null) + ' - Pago diretamente para '}
-							</Text>
-							<Image
-								style = {{width:37,height:29,marginLeft:3}}
-								source = {{uri:this.state.product? this.state.product.logo : null}}>
+							<View style = {{width:theme.width * 0.9, /* borderColor: 'black',borderWidth:1 */alignSelf:'center'}}>
+								<Text
+								style = {{color:'#8f8f8f', fontWeight:'bold',fontSize:19,marginBottom:4}}
+								>{'Valor:'}
+								</Text>
+								<View style = {{flexDirection:'row'}}>
+									<Text
+									style = {{fontWeight:'bold',fontSize:17}}
+									>{'R$ ' + (this.state.product ? this.state.product.price : '') + ' - Pago diretamente para '}
+									</Text>
+									<Image
+										style = {{width:37,height:29,marginLeft:3}}
+										source = {{uri:this.state.product? this.state.product.logo : null}}>
 
-							</Image>
+									</Image>
+								</View>
+								<View style = {{flexDirection:'row',marginTop:5}}>
+									<Text
+									style = {{fontWeight:'bold',fontSize:17}}
+									>{'R$ ' + (this.state.product? this.state.product.PicPayPrice : '') + ' - Pago pelo '}
+									</Text>
+									<Image
+										style = {{width:61,height:20,marginLeft:3,marginTop:5}}
+										source = {{uri:this.state.product? this.state.product.PicPayLogo : null}}>
+									</Image>
+								</View>
+							</View>
+							<View style = {{width:theme.width * 0.9, /* borderColor: 'black',borderWidth:1 */alignSelf:'center',marginTop:theme.height * 0.02}}>
+								<Text
+								style = {{color:'#8f8f8f',fontSize:15,textAlign: 'justify', lineHeight: 25}}
+								>{'Caracaterísticas escolhidas: Nenhuma por enquanto ' }</Text>
+							</View>
+
+							<View style = {{ width: theme.width * 0.9,alignSelf:'center',marginTop:theme.height*0.03}}>
+							<FatBottomedButton text = {'Comprar'} backgroundColor = {this.state.product? this.state.product.colors[0] : null}
+												color = {this.state.product? this.state.product.colors[1] : null }borderWidth = {0.1} height = {48}
+												/>
+							</View>
+							<View style = {{ width:theme.width * 0.9, alignSelf:'center',
+											padding:10, marginTop: theme.height * 0.03}}>
+								<Text style = {{fontWeight:'bold', fontSize:16,marginBottom:theme.height * 0.004}}>{'Descrição:'}</Text>
+								<Text style = {{flex:1,fontSize:15, textAlign: 'justify', lineHeight: 25,}}>{this.state.product? this.state.product.Description : null}</Text>
+							</View>
+							<View style = {{marginTop:theme.height * 0.04, padding:20, backgroundColor:this.state.product? this.state.product.colors[0] : null,
+									elevation: 8, }}>
+								<Text style = {{alignSelf:'center', fontSize:22,fontWeight:'bold'}}>{'Opções de Personalização'}</Text>
+							</View>
 						</View>
-						<View style = {{flexDirection:'row',marginTop:5}}>
-							<Text
-							style = {{fontWeight:'bold',fontSize:17}}
-							>{'R$ ' + (this.state.product? this.state.product.PicPayPrice:null) + ' - Pago pelo '}
-							</Text>
-							<Image
-								style = {{width:61,height:20,marginLeft:3,marginTop:5}}
-								source = {{uri:this.state.product? this.state.product.PicPayLogo : null}}>
-							</Image>
-						</View>
-					</View>
-					<View style = {{width:theme.width * 0.9, /* borderColor: 'black',borderWidth:1 */alignSelf:'center',marginTop:theme.height * 0.02}}>
-						<Text
-						style = {{color:'#8f8f8f',fontSize:15,textAlign: 'justify', lineHeight: 25}}
-						>{'Caracaterísticas escolhidas: Nenhuma por enquanto ' }</Text>
-					</View>
 
-					<View style = {{ width: theme.width * 0.9,alignSelf:'center',marginTop:theme.height*0.03}}>
-					<FatBottomedButton text = {'Comprar'} backgroundColor = {this.state.product? this.state.product.colors[0] : null}
-										color = {this.state.product? this.state.product.colors[1] : null }borderWidth = {0.1} height = {48}
-										/>
-					</View>
-					<View style = {{ width:theme.width * 0.9, alignSelf:'center',
-									padding:10, marginTop: theme.height * 0.03}}>
-						<Text style = {{fontWeight:'bold', fontSize:16,marginBottom:theme.height * 0.004}}>{'Descrição:'}</Text>
-						<Text style = {{flex:1,fontSize:15, textAlign: 'justify', lineHeight: 25,}}>{this.state.product? this.state.product.Description : null}</Text>
-					</View>
-					<View style = {{marginTop:theme.height * 0.04, padding:20, backgroundColor:this.state.product? this.state.product.colors[0] : null,
-							elevation: 8, }}>
-						<Text style = {{alignSelf:'center', fontSize:22,fontWeight:'bold'}}>{'Opções de Personalização'}</Text>
-					</View>
-				</View>
-				
-				
-				}
-				
+					}
                     data = {this.state.product ? this.state.product.customization : null}
 					renderItem={ ({item}) =>
-					
+
 						<View style = {{alignSelf:'center'}} >
 
-							<CustomizationOptions customizationCallback = {this.addCustomizationOption} productCustomization = {this.state.productCustomization} colors = {this.state.product.colors} custom = {item}/> 
+							<CustomizationOptions customizationCallback = {this.addCustomizationOption} productCustomization = {this.state.productCustomization} colors = {this.state.product.colors} custom = {item}/>
 
 						</View>
-						}
-						numColumns={2}
-                        keyExtractor={item => item.id}
-						onEndReachedThreshold={0.3}
-						
-						ListFooterComponent={ () => 
-							<View>
-								{this.state.product ? (
-									<View>
-										<Text style = {{color:'#8f8f8f',marginLeft:theme.width * 0.05}}>{'Preencha apenas os campos em que desejar escrita :'}</Text>
-										<View style = {{borderTopColor:'#8f8f8f',borderTopWidth:0.5,marginTop:theme.height * 0.005,paddingTop:theme.height * 0.02}}>
-											<CustomizationTextArea customization ={this.state.product.customization}/>
-										</View>
-									</View>) : null}
-							</View>
-							
-						
-						}
-						 
-                        />
+					}
+					numColumns={2}
+                    keyExtractor={item => item.id}
+					onEndReachedThreshold={0.3}
+					ListFooterComponent={ () =>
+						<View>
+							{this.state.product ? (
+								<View>
+									<Text style = {{color:'#8f8f8f',marginLeft:theme.width * 0.05}}>{'Preencha apenas os campos em que desejar escrita :'}</Text>
+									<View style = {{borderTopColor:'#8f8f8f',borderTopWidth:0.5,marginTop:theme.height * 0.005,paddingTop:theme.height * 0.02}}>
+										<CustomizationTextArea customization ={this.state.product.customization}/>
+									</View>
+								</View>) : null}
+						</View>
 
+					}
+                />
             	</View>
 			</View>
 		)
