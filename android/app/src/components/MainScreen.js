adc9a1a8672763d4872dbfedeb4670f759490ec8
GedorN <gedor.silvaneto@gmail.com>
2020-06-04 20:11:27 -0300

    Side drawer touch melhoria

--- android/app/src/components/MainScreen.js
+++ android/app/src/components/MainScreen.js
@@ -228,7 +228,7 @@ export default class MainScreen extends React.Component {
 						acceptPan={true}
 						negotiatePan={true}
 						panThreshold={0.25}
-						panOpenMask={0.1}
+						panOpenMask={0.04}
 					>
 						<View style={styles.header}>
 							<TouchableOpacity onPress={this.openModal.bind(this)}>
