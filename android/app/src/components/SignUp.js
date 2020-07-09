772220aceb4e5674ee204b68f1390694bfa57f0f
camilaantiqueira <camilaantiqueira93@gmail.com>
2020-07-08 22:38:19 -0300

    Correção margem

--- android/app/src/components/SignUp.js
+++ android/app/src/components/SignUp.js
@@ -569,7 +569,6 @@ const styles= StyleSheet.create({
 		flex: 1,
 		justifyContent: "center",
 		alignItems: "center",
-		marginTop: 22,
 		backgroundColor: 'rgba(0, 0, 0, 0.5)',
 		paddingTop: theme.height * 0.1,
 		marginTop: -(theme.height * 0.1)
